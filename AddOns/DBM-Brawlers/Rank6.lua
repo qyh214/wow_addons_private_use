c92996903330688fb932c60205fdba9874920979
qyh214 <qyh0214@qyh0214.com>
2019-09-11 13:50:36 +0800

    插件例行更新：DBM、DejaCharacterStats

--- AddOns/DBM-Brawlers/Rank6.lua
+++ AddOns/DBM-Brawlers/Rank6.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("BrawlRank6", "DBM-Brawlers")
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20190724002751")
+mod:SetRevision("20190725031458")
 --mod:SetModelID(39166)
 mod:SetZone()
 
--- AddOns/DBM-WorldEvents/Quests/Plants.lua
+++ AddOns/DBM-WorldEvents/Quests/Plants.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("PlantsVsZombies", "DBM-WorldEvents", 2)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20190724002751")
+mod:SetRevision("20190725031458")
 mod:SetZone(DBM_DISABLE_ZONE_DETECTION)
 
 mod:RegisterEvents(
